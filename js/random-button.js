269d0612f9a02c2d8874209c684548650bc94cc3
Alexandra Rezk <31907778+AlexandraRezk@users.noreply.github.com>
2019-07-17 17:46:59 -0400

    Initial Uploads
    Uploading index.html file
    Uploading Javascript to create filter buttons and random button
    Uploading DVD image library

---
+++ js/random-button.js
@@ -0,0 +1,30 @@
+(function(){
+	var $dvdContainer = $('.dvdGallery .dvdContainer'); 			//Stores all images
+	var $randomButton = $('#randomButton');						//Stores button elements
+	var myMovies = [];									//Create empty DVD array
+	
+	$dvdContainer.each(function(){						//Loop through DVDs
+	var dvds = this;									//Store DVDs in variable
+	myMovies.push(dvds);								//Stores DvDs in array
+	});
+	
+	//Buttons, event handlers, and filters go here
+	$('<span/>', {									//Create empty button
+		text: 'Random',									//Add text 'random'
+		class: 'active',								//make it active
+		click: function(){								//Add onclick handler to it
+			$(this)										//Get the clicked on button
+			.addClass('active')							//Add the class of active
+			.siblings()									//Get its siblings
+			.removeClass('active');						//Remove active from them
+			//Generate a random number based on the myMovies array
+			var randomDVD = 
+				myMovies[Math.floor(Math.random() * myMovies.length)]; 
+			$dvdContainer								//With all of the dvds
+			.hide()										//Hide them
+			//Find DVD in the array based on random number that was generated
+			.filter(randomDVD)
+			.show();									//Show just that dvd
+		}
+	}).appendTo($randomButton);								//Add to the buttons container
+}());
---
+++ js/test-crossfilter.js
@@ -0,0 +1,15 @@
+$('div.buttons').tagSort({
+	items:'.dvdContainer',
+	tagElement: 'span',
+	sortType: 'exclusive',
+});
+
+/*$().ready(function() {
+    $('div.buttons').each(function() {
+        $(this).tagSort({
+            items: '.dvdContainer',
+            tagElement: 'button',
+			sortType: 'exclusive',
+        });
+    });
+});*/
